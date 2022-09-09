61e6f0431bae64ea4aad55a739cd01e03f3214a2
aayan <aayan@gmail.com>
2022-09-09 16:32:06 +0530

    UpdateEmployee

--- src/main/java/com/springboot/controller/HomeController.java
+++ src/main/java/com/springboot/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,5 +46,11 @@ public class HomeController {
 	public ResponseEntity<Employee> getEmpById(@PathVariable("id") long employyeeid)
 	{
 		return new ResponseEntity<Employee>(employeeService.getEmpById(employyeeid),HttpStatus.OK);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> getEmpUpdate(@PathVariable("id") long id,@RequestBody Employee emp)
+	{
+		return new ResponseEntity<Employee>(employeeService.updateEmployee(emp,id),HttpStatus.OK);
 	}
 }
