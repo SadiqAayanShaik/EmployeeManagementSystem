9b92e75f7112c6f90d5eae7959fe01c9aee140a1
aayan <aayan@gmail.com>
2023-05-11 18:23:43 +0530

    ReactJs_BackEnd

--- src/main/java/com/springboot/exception/exceptionHandler.java
+++ src/main/java/com/springboot/exception/exceptionHandler.java
@@ -3,6 +3,8 @@ package com.springboot.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.UnexpectedTypeException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,15 +14,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class exceptionHandler {
 	
+	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public Map<String,String> handleInvalidException(MethodArgumentNotValidException e)
 	{
+		
 		Map<String,String> errorMap=new HashMap<>();
+		
 		e.getBindingResult().getFieldErrors().forEach(error->{
 			errorMap.put(error.getField(),error.getDefaultMessage());
 		});
 		return errorMap;
 	}
 
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public Map<String,String> handleBusinessException(ResourceNotFoundException ex)
+	{
+		Map<String,String> errorMap=new HashMap<>();
+		errorMap.put("errorMessge", ex.getMessage());
+		return errorMap;
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(UnexpectedTypeException.class)
+	public Map<String,String> handleDobException(UnexpectedTypeException ex)
+	{
+		Map<String,String> errorMap=new HashMap<>();
+		errorMap.put("errorMessge", ex.getMessage());
+		return errorMap;
+	}
 }
--- src/main/java/com/springboot/model/Employee.java
+++ src/main/java/com/springboot/model/Employee.java
@@ -1,15 +1,19 @@
 package com.springboot.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
@@ -36,7 +40,7 @@ public class Employee {
 	@NotEmpty
 	private String dob;
 	
-	@NotEmpty
+	@NotEmpty(message = "Enter Male, Female or Others")
 	private String gender;
 	
 	@NotEmpty
@@ -92,13 +96,15 @@ public class Employee {
 		return dob;
 	}
 	public void setDob(String dob) {
+		
 		this.dob = dob;
 	}
 	public String getGender() {
 		return gender;
 	}
 	public void setGender(String gender) {
-		this.gender = gender;
+		
+	 this.gender=gender;
 	}
 	public String getPhone() {
 		return phone;
@@ -113,6 +119,9 @@ public class Employee {
 		this.salary = salary;
 	}
 	
-	
+	public enum gender
+	{
+		male,female,others
+	}
 
 }
